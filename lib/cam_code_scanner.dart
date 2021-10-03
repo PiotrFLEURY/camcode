81103fc2bbd1600a61d72e3af404bca2b5344c12
piotr <piotr.fleury@gmail.com>
2021-10-03 16:45:06 +0200

    feat(pointofinterest): add point of interest

--- lib/cam_code_scanner.dart
+++ lib/cam_code_scanner.dart
@@ -9,22 +9,22 @@ import 'package:flutter/services.dart';
 /// Ask your favorite javascript library
 /// to identify a barcode in the current picture
 class CamCodeScanner extends StatefulWidget {
-  // shows the current analysing picture
+  /// shows the current analysing picture
   final bool showDebugFrames;
 
-  // call back to trigger on barcode result
+  /// call back to trigger on barcode result
   final Function onBarcodeResult;
 
-  // width dimension
+  /// width dimension
   final double width;
 
-  // height dimension
+  /// height dimension
   final double height;
 
-  // delay between to picture analysis
+  /// delay between to picture analysis
   final int refreshDelayMillis;
 
-  // controller to control the camera from outside
+  /// controller to control the camera from outside
   final CamCodeScannerController? controller;
 
   /// Camera barcode scanner widget
@@ -47,15 +47,19 @@ class CamCodeScanner extends StatefulWidget {
 }
 
 class _CamCodeScannerState extends State<CamCodeScanner> {
-  // communication channel between widget and platform code
+  /// communication channel between widget and platform code
   final MethodChannel channel = MethodChannel('camcode');
-  // Webcam widget to insert into the tree
+
+  /// Webcam widget to insert into the tree
   late Widget _webcamWidget;
-  // Debug frame Image widget to insert into the tree
+
+  /// Debug frame Image widget to insert into the tree
   late Widget _imageWidget;
-  // The barcode result
+
+  /// The barcode result
   String barcode = '';
-  // Used to know if camera is loading or initialized
+
+  /// Used to know if camera is loading or initialized
   bool initialized = false;
 
   @override
@@ -75,8 +79,6 @@ class _CamCodeScannerState extends State<CamCodeScanner> {
 
   /// Calls the platform initialization and wait for result
   Future<void> initialize() async {
-    widget.controller?._channel = channel;
-
     final time = await channel.invokeMethod(
       'initialize',
       [
@@ -87,16 +89,22 @@ class _CamCodeScannerState extends State<CamCodeScanner> {
     );
 
     // Create video widget
-    _webcamWidget =
-        HtmlElementView(key: UniqueKey(), viewType: 'webcamVideoElement$time');
+    _webcamWidget = HtmlElementView(
+      key: UniqueKey(),
+      viewType: 'webcamVideoElement$time',
+    );
 
-    _imageWidget = HtmlElementView(viewType: 'imageElement');
+    _imageWidget = HtmlElementView(
+      viewType: 'imageElement',
+    );
 
+    // Set the initialized flag
     setState(() {
       initialized = true;
     });
 
     _waitForResult();
+    widget.controller?._channelCompleter.complete(channel);
   }
 
   /// Waits for the platform completer result
@@ -108,6 +116,7 @@ class _CamCodeScannerState extends State<CamCodeScanner> {
     });
   }
 
+  /// Method called when a barcode is detected
   Future<void> onBarcodeResult(String _barcode) async {
     setState(() {
       barcode = _barcode;
@@ -125,31 +134,61 @@ class _CamCodeScannerState extends State<CamCodeScanner> {
         },
         child: Builder(
           builder: (context) => Center(
-            child: Stack(
-              children: <Widget>[
-                initialized
-                    ? SizedBox(
+            child: initialized
+                ? Stack(
+                    children: <Widget>[
+                      SizedBox(
                         width: widget.width,
                         height: widget.height,
                         child: _webcamWidget,
-                      )
-                    : CircularProgressIndicator(),
-                !widget.showDebugFrames
-                    ? Container()
-                    : SizedBox(
-                        width: 100,
-                        height: 100,
-                        child: _imageWidget,
                       ),
-                Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(barcode),
-                  ),
-                ),
-              ],
-            ),
+                      if (widget.showDebugFrames)
+                        Container(
+                          width: widget.width,
+                          height: widget.height,
+                          color: Colors.black.withOpacity(0.8),
+                          child: Text(''),
+                        ),
+                      Positioned(
+                        top: (widget.height / 2) - (widget.height * .1),
+                        left: (widget.width * .1),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.green,
+                              width: 1,
+                            ),
+                          ),
+                          child: SizedBox(
+                            width: widget.width * .8,
+                            height: widget.height * .2,
+                            child: widget.showDebugFrames
+                                ? _imageWidget
+                                : Container(),
+                          ),
+                        ),
+                      ),
+                      Center(
+                        child: CustomPaint(
+                          size: Size(
+                            widget.width * .5,
+                            widget.height * .2,
+                          ),
+                          painter: _ScannerLine(
+                            color: Colors.red,
+                          ),
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(barcode),
+                        ),
+                      ),
+                    ],
+                  )
+                : CircularProgressIndicator(),
           ),
         ),
       ),
@@ -157,15 +196,62 @@ class _CamCodeScannerState extends State<CamCodeScanner> {
   }
 }
 
+/// Custom painter to draw the scanner line
+class _ScannerLine extends CustomPainter {
+  /// Color of the line
+  final Color color;
+
+  _ScannerLine({
+    this.color = Colors.white,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = color
+      ..strokeWidth = 1.0
+      ..style = PaintingStyle.stroke;
+
+    canvas.drawLine(
+      Offset(0, size.height / 2),
+      Offset(size.width, size.height / 2),
+      paint,
+    );
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
 /// Controller to control the camera from outside
 class CamCodeScannerController {
-  // Channel to communicate with the platform code
-  late MethodChannel _channel;
+  /// Channel to communicate with the platform code
+  final Completer<MethodChannel> _channelCompleter = Completer();
 
-  // Invoke this method to close the camera and release all resources
+  /// Invoke this method to close the camera and release all resources
   Future<void> releaseResources() async {
+    final _channel = await _channelCompleter.future;
     return _channel.invokeMethod(
       'releaseResources',
+    );
+  }
+
+  /// Waits for the device list completer result
+  Future<List<String>> fetchDeviceList() async {
+    final _channel = await _channelCompleter.future;
+    final devices =
+        await _channel.invokeMethod<List<dynamic>?>('fetchDeviceList');
+    return devices?.map((e) => e.toString()).toList() ?? [];
+  }
+
+  /// Selects the device with the given device name
+  Future<void> selectDevice(String device) async {
+    final _channel = await _channelCompleter.future;
+    return _channel.invokeMethod(
+      'selectDevice',
+      device,
     );
   }
 }
